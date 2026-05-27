7b785ff7a8a2af8ed43296036371c9b3f769384b
Firdaus Abdullah <87492065+FirdausAbdullah@users.noreply.github.com>
2026-05-27 22:10:08 +0800

    Add contents to Home tab.

--- src/homeModule.js
+++ src/homeModule.js
@@ -2,5 +2,15 @@
 export function populateHome(){
     let contentDiv = document.querySelector("#content");
     contentDiv.innerHTML = '';
-    contentDiv.textContent = "Home Page";
+
+    let h1 = document.createElement("h1");
+    h1.className = "homeHeader";
+    h1.textContent = "Apek Mee Tarik";
+    contentDiv.appendChild(h1);
+
+    let p = document.createElement("p");
+    p.className = "homeDescription";
+    p.textContent = "Mee Tarik is a very delicious delicacy of the Chinese. Come and try";
+    contentDiv.appendChild(p);
 }
+
