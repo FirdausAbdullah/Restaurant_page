0674d33b0e8fe9526f3ac1c9f92e082ece4d8c78
Firdaus Abdullah <87492065+FirdausAbdullah@users.noreply.github.com>
2026-05-28 23:11:16 +0800

    Modify menu module to add food list.

--- src/menuModule.js
+++ src/menuModule.js
@@ -2,5 +2,32 @@
 export function populateMenu(){
     let contentDiv = document.querySelector("#content");
     contentDiv.innerHTML = '';
-    contentDiv.textContent = "Menu Page";
+
+
+    let menuHeader = document.createElement("h1");
+    menuHeader.classList = "menuHeader";
+    menuHeader.textContent = "Our Menu";
+    contentDiv.appendChild(menuHeader);
+
+    let menuDiv = document.createElement("div");
+    menuDiv.classList = "menuDiv";
+    contentDiv.appendChild(menuDiv);
+
+    addMenu(menuDiv,"Braised Beef Ramen","15.00");
+    addMenu(menuDiv,"Beef Ramen","12.00");
+    addMenu(menuDiv,"Dry Mee Tarik","13.00");
+    addMenu(menuDiv,"Chicken Dumpling","10.00");
+}
+
+function addMenu(addInto,food,price){
+    let foodDiv = document.createElement("div");
+    let foodSpan = document.createElement("span");
+    let priceSpan = document.createElement("span");
+    foodSpan.textContent = food;
+    priceSpan.textContent = price;
+
+    foodDiv.appendChild(foodSpan);
+    foodDiv.appendChild(priceSpan);
+
+    addInto.appendChild(foodDiv);
 }
